76e34524f223357c53a6d00de6110b3f254ac7bf
iampkiron <iampkiron@naver.com>
2022-04-08 18:36:38 +0900

    3nd commit

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ addButton.addEventListener("click", (e) => {
 
   // like
   const like = document.createElement("span");
-  like.classList.add("material-icons");
+  like.classList.add("material-icons", "like");
   like.innerText = "favorite_border";
 
   // Text
@@ -39,11 +39,23 @@ addButton.addEventListener("click", (e) => {
 
   checkIcon.addEventListener("click", (e) => {
     const target = e.target.parentNode.previousSibling;
-    const checkIcon =
-      target.style.textDecoration === "line-through"
-        ? (target.style.textDecoration = "")
-        : (target.style.textDecoration = "line-through");
-    target.style.textDecoration = checkIcon;
+
+    const text =
+      target.innerText === "check_circle"
+        ? (target.innerText = "check")
+        : (target.innerText = "check_circle");
+
+    const checkIcon = e.target.classList.contains("doneIcon")
+      ? e.target.classList.remove("doneIcon")
+      : e.target.classList.add("doneIcon");
+
+    const checkText = e.target.classList.contains("doneIcon")
+      ? target.classList.add("done")
+      : target.classList.remove("done");
+
+    e.target.classList.add(checkIcon);
+    e.target.innerText = text;
+    target.classList.add(checkText);
   });
 
   clearIcon.addEventListener("click", (e) => {
@@ -56,7 +68,7 @@ addButton.addEventListener("click", (e) => {
   checkIcon.classList.add("clearIcon");
   clearIcon.classList.add("material-icons");
   clearIcon.classList.add("checkIcon");
-  checkIcon.innerText = "check_circle";
+  checkIcon.innerText = "check";
   clearIcon.innerText = "delete";
   manage.appendChild(checkIcon);
   manage.appendChild(clearIcon);
